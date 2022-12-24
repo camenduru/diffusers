86b3c7663f380db3c54aa919d08999f509207844
camenduru <54370274+camenduru@users.noreply.github.com>
2022-12-24 06:09:13 +0300

    new converter

--- src/diffusers/modeling_pytorch_flax_utils.py
+++ src/diffusers/modeling_pytorch_flax_utils.py
@@ -65,59 +65,55 @@ def load_flax_checkpoint_in_pytorch_model(pt_model, model_file):
 
 
 def load_flax_weights_in_pytorch_model(pt_model, flax_state):
+    """Load flax checkpoints in a PyTorch model"""
 
+    flax_state = jax.tree_util.tree_map(lambda x: jax.device_put(x, jax.devices("cpu")[0]), flax_state)
+    flax_state = flatten_dict(flax_state)
 
-    return pt_model
+    with open("/content/flax_nested_state_dict_keys_flattened.txt", "w") as f:
+        for key in sorted(flax_state.keys()):
+            f.write(key + "\n")
 
-    # """Load flax checkpoints in a PyTorch model"""
+    return model, unflatten_dict(state)
 
-    # flax_state = jax.tree_util.tree_map(lambda x: jax.device_put(x, jax.devices("cpu")[0]), flax_state)
-    # flax_state = flatten_dict(flax_state)
+    try:
+        import torch  # noqa: F401
+    except ImportError:
+        logger.error(
+            "Loading a Flax weights in PyTorch, requires both PyTorch and Flax to be installed. Please see"
+            " https://pytorch.org/ and https://flax.readthedocs.io/en/latest/installation.html for installation"
+            " instructions."
+        )
+        raise
 
-    # with open("/content/flax_nested_state_dict_keys_flattened.txt", "w") as f:
-    #     for key in sorted(flax_state.keys()):
-    #         f.write(key + "\n")
+    # check if we have bf16 weights
+    is_type_bf16 = flatten_dict(jax.tree_util.tree_map(lambda x: x.dtype == jnp.bfloat16, flax_state)).values()
+    if any(is_type_bf16):
+        # convert all weights to fp32 if the are bf16 since torch.from_numpy can-not handle bf16
+        # and bf16 is not fully supported in PT yet.
+        logger.warning(
+            "Found ``bfloat16`` weights in Flax model. Casting all ``bfloat16`` weights to ``float32`` "
+            "before loading those in PyTorch model."
+        )
+        flax_state = jax.tree_util.tree_map(
+            lambda params: params.astype(np.float32) if params.dtype == jnp.bfloat16 else params, flax_state
+        )
 
-    # return model, unflatten_dict(state)
+    pt_model.base_model_prefix = ""
 
-    # try:
-    #     import torch  # noqa: F401
-    # except ImportError:
-    #     logger.error(
-    #         "Loading a Flax weights in PyTorch, requires both PyTorch and Flax to be installed. Please see"
-    #         " https://pytorch.org/ and https://flax.readthedocs.io/en/latest/installation.html for installation"
-    #         " instructions."
-    #     )
-    #     raise
+    flax_state_dict = flatten_dict(flax_state)
+    pt_model_dict = pt_model.state_dict()
 
-    # # check if we have bf16 weights
-    # is_type_bf16 = flatten_dict(jax.tree_util.tree_map(lambda x: x.dtype == jnp.bfloat16, flax_state)).values()
-    # if any(is_type_bf16):
-    #     # convert all weights to fp32 if the are bf16 since torch.from_numpy can-not handle bf16
-    #     # and bf16 is not fully supported in PT yet.
-    #     logger.warning(
-    #         "Found ``bfloat16`` weights in Flax model. Casting all ``bfloat16`` weights to ``float32`` "
-    #         "before loading those in PyTorch model."
-    #     )
-    #     flax_state = jax.tree_util.tree_map(
-    #         lambda params: params.astype(np.float32) if params.dtype == jnp.bfloat16 else params, flax_state
-    #     )
+    load_model_with_head_into_base_model = (pt_model.base_model_prefix in flax_state) and (
+        pt_model.base_model_prefix not in set([k.split(".")[0] for k in pt_model_dict.keys()])
+    )
+    load_base_model_into_model_with_head = (pt_model.base_model_prefix not in flax_state) and (
+        pt_model.base_model_prefix in set([k.split(".")[0] for k in pt_model_dict.keys()])
+    )
 
-    # pt_model.base_model_prefix = ""
-
-    # flax_state_dict = flatten_dict(flax_state)
-    # pt_model_dict = pt_model.state_dict()
-
-    # load_model_with_head_into_base_model = (pt_model.base_model_prefix in flax_state) and (
-    #     pt_model.base_model_prefix not in set([k.split(".")[0] for k in pt_model_dict.keys()])
-    # )
-    # load_base_model_into_model_with_head = (pt_model.base_model_prefix not in flax_state) and (
-    #     pt_model.base_model_prefix in set([k.split(".")[0] for k in pt_model_dict.keys()])
-    # )
-
-    # # keep track of unexpected & missing keys
-    # unexpected_keys = []
-    # missing_keys = set(pt_model_dict.keys())
+    # keep track of unexpected & missing keys
+    unexpected_keys = []
+    missing_keys = set(pt_model_dict.keys())
 
     # for flax_key_tuple, flax_tensor in flax_state_dict.items():
     #     has_base_model_prefix = flax_key_tuple[0] == pt_model.base_model_prefix
@@ -159,34 +155,64 @@ def load_flax_weights_in_pytorch_model(pt_model, flax_state):
     #         # weight is not expected by PyTorch model
     #         unexpected_keys.append(flax_key)
 
-    # pt_model.load_state_dict(pt_model_dict)
+    for flax_key_tuple, flax_tensor in sorted(flax_vae_nested_state_dict_flattened.items()):
+        flax_key_tuple_array = flax_key_tuple.split('.')
+        if flax_key_tuple_array[-1] == "kernel" and flax_tensor.ndim == 4:
+            flax_key_tuple_array = flax_key_tuple_array[:-1] + ["weight"]
+            flax_tensor = jnp.transpose(flax_tensor, (3, 2, 0, 1))
 
-    # # re-transform missing_keys to list
-    # missing_keys = list(missing_keys)
+        if flax_key_tuple_array[-1] == "scale":
+            flax_key_tuple_array = flax_key_tuple_array[:-1] + ["weight"]
 
-    # if len(unexpected_keys) > 0:
-    #     logger.warning(
-    #         "Some weights of the Flax model were not used when initializing the PyTorch model"
-    #         f" {pt_model.__class__.__name__}: {unexpected_keys}\n- This IS expected if you are initializing"
-    #         f" {pt_model.__class__.__name__} from a Flax model trained on another task or with another architecture"
-    #         " (e.g. initializing a BertForSequenceClassification model from a FlaxBertForPreTraining model).\n- This"
-    #         f" IS NOT expected if you are initializing {pt_model.__class__.__name__} from a Flax model that you expect"
-    #         " to be exactly identical (e.g. initializing a BertForSequenceClassification model from a"
-    #         " FlaxBertForSequenceClassification model)."
-    #     )
-    # else:
-    #     logger.warning(f"All Flax model weights were used when initializing {pt_model.__class__.__name__}.\n")
-    # if len(missing_keys) > 0:
-    #     logger.warning(
-    #         f"Some weights of {pt_model.__class__.__name__} were not initialized from the Flax model and are newly"
-    #         f" initialized: {missing_keys}\nYou should probably TRAIN this model on a down-stream task to be able to"
-    #         " use it for predictions and inference."
-    #     )
-    # else:
-    #     logger.warning(
-    #         f"All the weights of {pt_model.__class__.__name__} were initialized from the Flax model.\n"
-    #         "If your task is similar to the task the model of the checkpoint was trained on, "
-    #         f"you can already use {pt_model.__class__.__name__} for predictions without further training."
-    #     )
+        for i, flax_key_tuple_string in enumerate(flax_key_tuple_array):
+            flax_key_tuple_array[i] = flax_key_tuple_string.replace('_0', '.0').replace('_1', '.1').replace('_2', '.2')
 
-    # return pt_model
+        flax_key = ".".join(flax_key_tuple_array)
+
+        if flax_key in pt_model_dict:
+            if flax_tensor.shape != pt_model_dict[flax_key].shape:
+                raise ValueError(
+                    f"Flax checkpoint seems to be incorrect. Weight {flax_key_tuple} was expected "
+                    f"to be of shape {pt_model_dict[flax_key].shape}, but is {flax_tensor.shape}."
+                )
+            else:
+                # add weight to pytorch dict
+                flax_tensor = np.asarray(flax_tensor) if not isinstance(flax_tensor, np.ndarray) else flax_tensor
+                pt_model_dict[flax_key] = torch.from_numpy(flax_tensor)
+                # remove from missing keys
+                missing_keys.remove(flax_key)
+        else:
+            # weight is not expected by PyTorch model
+            unexpected_keys.append(flax_key)
+  
+    pt_model.load_state_dict(pt_model_dict)
+
+    # re-transform missing_keys to list
+    missing_keys = list(missing_keys)
+
+    if len(unexpected_keys) > 0:
+        logger.warning(
+            "Some weights of the Flax model were not used when initializing the PyTorch model"
+            f" {pt_model.__class__.__name__}: {unexpected_keys}\n- This IS expected if you are initializing"
+            f" {pt_model.__class__.__name__} from a Flax model trained on another task or with another architecture"
+            " (e.g. initializing a BertForSequenceClassification model from a FlaxBertForPreTraining model).\n- This"
+            f" IS NOT expected if you are initializing {pt_model.__class__.__name__} from a Flax model that you expect"
+            " to be exactly identical (e.g. initializing a BertForSequenceClassification model from a"
+            " FlaxBertForSequenceClassification model)."
+        )
+    else:
+        logger.warning(f"All Flax model weights were used when initializing {pt_model.__class__.__name__}.\n")
+    if len(missing_keys) > 0:
+        logger.warning(
+            f"Some weights of {pt_model.__class__.__name__} were not initialized from the Flax model and are newly"
+            f" initialized: {missing_keys}\nYou should probably TRAIN this model on a down-stream task to be able to"
+            " use it for predictions and inference."
+        )
+    else:
+        logger.warning(
+            f"All the weights of {pt_model.__class__.__name__} were initialized from the Flax model.\n"
+            "If your task is similar to the task the model of the checkpoint was trained on, "
+            f"you can already use {pt_model.__class__.__name__} for predictions without further training."
+        )
+
+    return pt_model
